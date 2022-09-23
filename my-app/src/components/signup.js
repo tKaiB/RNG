0138882a1555fb70bc56caf40486030fd654bfcc
Vignesh Ezhil <86967110+viggy2000@users.noreply.github.com>
2022-09-23 23:44:22 +0800

    new update log in page

--- my-app/src/components/signup.js
+++ my-app/src/components/signup.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Grid , TextField} from '@material-ui/core'
+import { Button, Checkbox, FormGroup, Grid , TextField , FormControlLabel, InputAdornment} from '@material-ui/core'
+import {AccountCircle , LockRounded} from '@material-ui/icons'
+
+
 
 function SignUp(){
 
@@ -15,18 +18,79 @@ function SignUp(){
                 </Grid>
 
 
-                <Grid container item xs={12} sm={6}  style = {{padding :10}}>
-                    <div />
-                
-                    <div>
-                        <TextField label= 'Username' margin ='normal'/>
-                        <TextField label= 'Password' margin ='normal'/>
+                <Grid item xs={12} sm={6} alignItems= "left" direction = "column" justify = "space-between" style = {{paddingLeft :50}}>
+                    <div style ={{alignItems: "left"}}>
+                        <Grid container direction = "column">
+                            <Grid item xs={4}>
+                                <h2 style={{padding:10 , paddingLeft :0}}>Sign in</h2>
+                            </Grid>
 
-                    </div>
+                            <Grid item xs={4}>
+                                <p style={{margin: 0 , paddingBottom:7}}>If you dont have an account</p>
+                            </Grid>
+
+                            <Grid item xs={4}>
+                                <p style={{margin: 0}}>You can register here</p>
+                            </Grid>
+
+                        </Grid>
     
 
 
+                        
+                    </div>
 
+
+                    <div style ={{display : "flex" , flexDirection : "column" , maxWidth :500 , minWidth: 300 , paddingTop : 30 , paddingBottom: 20}} >
+                        <TextField 
+                        label= 'Email' 
+                        margin ='normal'
+                        defaultValue= "Enter your Email Address"
+                        InputProps={{startAdornment: (<InputAdornment position = "start"> <AccountCircle /></InputAdornment>),}}
+                        
+                        
+                        />
+
+                        
+                        <TextField 
+                        label= 'Password'
+                         margin ='normal'
+                         defaultValue= "Enter your Password"
+
+                         InputProps={{startAdornment: (<InputAdornment position = "start"> <LockRounded /></InputAdornment>),}}
+                        
+                         
+                         />
+                        <Grid container style={{paddingTop:10}}>
+                            <Grid item xs={8}>
+                                <FormGroup>
+                                <FormControlLabel control={<Checkbox defaultChecked />} label="Remember me " />
+                                </FormGroup>
+                            </Grid>
+
+                            <Grid item xs={4} alignItems = "end" >
+                                <Button style={{fontFamily :"Inter" , fontSize: "15px" , textTransform : "none" , paddingRight : 0 , textAlign:"right"}}>Forgot your Password</Button>
+                            </Grid>
+                           
+                           
+                            
+                        </Grid>
+
+    
+                        
+
+                    </div>
+
+         
+
+                    <div style={{display : "flex" , flexDirection : "column" , maxWidth :500 , minWidth: 300}}>
+                        <Button  variant = "contained" style={{textTransform :"none" , backgroundColor: "#000000" , color:"#ffffff"}}>
+                            Login 
+                        </Button>
+
+                    </div>
+
+    
 
                 </Grid>
 
