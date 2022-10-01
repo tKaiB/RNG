c7a08704b18957dbb7a4328b856414702ffe4144
Zisuan <101612936+Zisuan@users.noreply.github.com>
2022-10-01 20:27:12 +0800

    Update navbar.js

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -1,6 +1,48 @@
+import React ,{ useState , useRef} from 'react'
+import "./styles.css"
+import { Link , useNavigate } from "react-router-dom"
+import { UserAuth } from '../contexts/AuthContext'
+
 function NavBar(){
-    return (
-        <h1 style={{backgroundColor :'#FFFFFF'}}>This is a nav bar</h1>
-    )
+    const {signIn} = UserAuth();
+    const [error,setError] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const navigate = useNavigate()
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault()
+        setError('')
+        try {
+            await signIn(emailRef.current.value, passwordRef.current.value)
+            navigate('/account')
+            
+        } catch (e) {
+            setError(e.message)
+            console.log(e.message)
+        }
+    }
+    return <nav className = "nav">
+          <a onClick={handleSubmit} className="site-title">RNG</a>
+          <ul>
+            <li>
+                <Link to="/Aboutus">About Us</Link>
+            </li>
+            <li>
+                <Link to="/FAQ">FAQ</Link>
+            </li>
+            <li>
+                <button className="btn" onClick = {() => navigate('/')}>Sign Up</button>
+            </li>
+            <li>
+                <button className="btn" onClick = {() => navigate('/Signin')}>Sign In</button>
+                
+            </li>
+          </ul>
+    
+    </nav>
+        
 }
 export default NavBar
