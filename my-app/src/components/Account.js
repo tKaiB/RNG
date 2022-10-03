19bda96c55ae26c095b09dcec2a8abcb35139555
Vignesh Ezhil <86967110+viggy2000@users.noreply.github.com>
2022-10-03 20:52:55 +0800

    updated

--- my-app/src/components/Account.js
+++ my-app/src/components/Account.js
@@ -23,16 +23,19 @@ function Account() {
 
   return (
     <div>
-      <div>
+      <div style={{paddingBottom:10}}>
         <ResponsiveAppBar />
       </div>
       <div>
-        <Grid container>
-          <div>
-            <Grid style={{ padding: 0 }}>
+        <Grid container direction = {'row'} alignItems="flex-start" justify = 'flex-start' spacing={1}>
+            <Grid xs = {4} sm ={4} style={{ padding: 0 }}>
               <SideBar />
             </Grid>
-          </div>
+
+            <Grid xs={6} sm ={4}>
+              <h1>user email : {user && user.uid}</h1>
+            </Grid>
+
         </Grid>
 
         {/* <Button onClick={handleLogout} type="submit">
