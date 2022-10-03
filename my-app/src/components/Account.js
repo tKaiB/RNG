bea6e1d9540606af5ae5e35346c9e4e691b336c1
Zisuan <zisuanlim@gmail.com>
2022-10-03 15:25:11 +0800

    updated

--- my-app/src/components/Account.js
+++ my-app/src/components/Account.js
@@ -1,31 +1,30 @@
-import React from 'react'
-import { Button} from '@material-ui/core'
-import { useNavigate } from 'react-router-dom';
-import { UserAuth } from '../contexts/AuthContext'
+import React from "react";
+import { Button } from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
+import { UserAuth } from "../contexts/AuthContext";
+import Sidebar from "./Sidebar";
 
-function Account(){
+function Account() {
+  const { user, logout } = UserAuth();
+  const navigate = useNavigate();
 
-    const { user , logout} = UserAuth()
-    const navigate = useNavigate()
-
-    const handleLogout = async () =>{
-        try {
-            await logout()
-            navigate('/')
-            
-        } catch (e) {
-            console.log(e.message)
-        }
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/");
+    } catch (e) {
+      console.log(e.message);
     }
+  };
 
-
-    return (
-        <div>
-            <h1>Successfully Loggged in </h1>
-            <Button onClick = {handleLogout} type="submit">
-                Log out 
-            </Button>
-        </div>
-    )
+  return (
+    <div>
+      <sideBar />
+      <h1>Successfully Loggged in </h1>
+      <Button onClick={handleLogout} type="submit">
+        Log out
+      </Button>
+    </div>
+  );
 }
-export default Account
+export default Account;
