ed1254ed4b4282b42723b3cecaae009b83005f6d
Zisuan <zisuanlim@gmail.com>
2022-10-03 18:29:36 +0800

    Updated

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -1,52 +1,90 @@
-import React, { useState, useRef } from "react";
-import "./styles.css";
-import { Link, useNavigate } from "react-router-dom";
-import { UserAuth } from "../contexts/AuthContext";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-function NavBar() {
-  const { signIn } = UserAuth();
-  const [error, setError] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const navigate = useNavigate();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    try {
-      await signIn(emailRef.current.value, passwordRef.current.value);
-      navigate("/account");
-    } catch (e) {
-      setError(e.message);
-      console.log(e.message);
-    }
-  };
+export default function NavBar() {
   return (
-    <nav className="nav">
-      <a onClick={handleSubmit} className="site-title">
-        RNG
-      </a>
-      <ul>
-        <li>
-          <Link to="/aboutus">About Us</Link>
-        </li>
-        <li>
-          <Link to="/FAQ">FAQ</Link>
-        </li>
-        <div>
-          <button className="btn" onClick={() => navigate("/SignUp")}>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{
+          backgroundColor: "aliceblue",
+          color: "black",
+          display: "flex",
+          padding: "0 1rem",
+          fontFamily: "'Times New Roman', Times, serif",
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+            RNG
+          </Typography>
+          <Typography
+            variant="h6"
+            component="a"
+            href="/Aboutus"
+            sx={{
+              color: "black",
+              textDecoration: "none",
+              listStyle: "none",
+              display: "flex",
+              padding: "1rem",
+            }}
+          >
+            About Us
+          </Typography>
+          <Typography
+            variant="h6"
+            component="a"
+            href="/Faq"
+            sx={{
+              color: "black",
+              textDecoration: "none",
+              listStyle: "none",
+              display: "flex",
+              padding: "1rem",
+            }}
+          >
+            FAQ
+          </Typography>
+          <Button
+            color="inherit"
+            href="/SignUp"
+            sx={{
+              backgroundColor: "deepskyblue",
+              borderRadius: "25px",
+              color: "white",
+              fontFamily: "'Courier New', Courier, monospace",
+              fontSize: "1rem",
+              alignItems: "center",
+              padding: "0.5rem 1rem",
+              border: "none",
+              margin: "1rem",
+            }}
+          >
             Sign Up
-          </button>
-        </div>
-        <div>
-          <button className="btn" onClick={() => navigate("/")}>
+          </Button>
+          <Button
+            color="inherit"
+            href="/"
+            sx={{
+              backgroundColor: "deepskyblue",
+              borderRadius: "25px",
+              color: "white",
+              fontFamily: "'Courier New', Courier, monospace",
+              fontSize: "1rem",
+              alignItems: "center",
+              padding: "0.5rem 1rem",
+              border: "none",
+            }}
+          >
             Sign In
-          </button>
-        </div>
-      </ul>
-    </nav>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
-export default NavBar;
