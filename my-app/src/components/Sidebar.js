bce6628d68d751681213566f3b7964f47fa3772d
tKaiB <86958913+tKaiB@users.noreply.github.com>
2022-10-03 17:52:46 +0800

    Update to Sidebar

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,41 +1,40 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
-
 import HomeIcon from '@mui/icons-material/Home';
 import { ListItem, ListItemButton, ListItemText } from '@mui/material';
 import List from '@mui/material/List';
-export default function SideBar() {
-  
-  return (
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
+function SideBar() {
+  const [view, setView] = React.useState('Home');
+
+  const handleChange = (event, nextView) => {
+    setView(nextView);
+  };
+  return(
     <Grid>
-      <List>
-        <ListItem>
-          <ListItemButton>
-            <HomeIcon />
-            <ListItemText primary={"Home"} />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton>
-            <HomeIcon />
-            <ListItemText primary={"Calorie Intake"} />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton>
-            <HomeIcon />
-            <ListItemText primary={"Recipe Generator"} />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton>
-            <HomeIcon />
-            <ListItemText primary={"BMI Input"} />
-          </ListItemButton>
-        </ListItem>
-      </List>
+        <ToggleButtonGroup
+          orientation="vertical"
+          value={view}
+          exclusive
+          onChange={handleChange}
+          fullWidth
+        >
+          <ToggleButton value="Home">
+            <HomeIcon />Home
+          </ToggleButton>
+          <ToggleButton value="Calorie Intake">
+            <HomeIcon />Calorie Intake
+          </ToggleButton>
+          <ToggleButton value="Recipe Generator">
+            <HomeIcon />Recipe Generator
+          </ToggleButton>
+          <ToggleButton value="BMI Input">
+            <HomeIcon />BMI Input
+          </ToggleButton>
+        </ToggleButtonGroup>
     </Grid>
-
-
   )
 }
+export default SideBar
