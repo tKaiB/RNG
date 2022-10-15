8f42c876145f3c9662a48d074726bffd80c95d3a
Vignesh Ezhil <86967110+viggy2000@users.noreply.github.com>
2022-10-16 01:28:22 +0800

    fixed structuring

--- my-app/src/components/AccountFaq.js
+++ my-app/src/components/AccountFaq.js
@@ -1,6 +1,6 @@
 import React from "react";
 import AccountNavBar from "./AccountNavBar";
-import "./styles.css";
+import "../pages/styles.css";
 import { Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
