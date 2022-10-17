6296cc93560bacf12e6241c4e0d730f56f0d699a
Zisuan <zisuanlim@gmail.com>
2022-10-17 23:26:02 +0800

    Update firebase.js

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage();
+export const storage = getStorage(app);
 export default app;
 
 // Storage
