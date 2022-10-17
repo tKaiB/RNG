ddb47da61a234d18562b28fc855f6ea73fe5f51e
Zisuan <zisuanlim@gmail.com>
2022-10-17 23:20:41 +0800

    Added profile picture

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,12 +13,25 @@ const firebaseConfig = {
   projectId: "rng-bbef4",
   storageBucket: "rng-bbef4.appspot.com",
   messagingSenderId: "939031468778",
-  appId: "1:939031468778:web:fe33caf8a370e4edfdd593"
+  appId: "1:939031468778:web:fe33caf8a370e4edfdd593",
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export default app 
+export const storage = getStorage();
+export default app;
+
+// Storage
+export async function upload(file, user, setLoading) {
+  const fileRef = ref(storage, user.uid + ".png");
+  setLoading(true);
+  const snapshot = await uploadBytes(fileRef, file);
+
+  const photoURL = await getDownloadURL(fileRef);
+
+  updateProfile(user, { photoURL });
+  setLoading(false);
+  alert("Uploaded file!");
+}
--- my-app/src/pages/Userprofile.js
+++ my-app/src/pages/Userprofile.js
@@ -2,84 +2,191 @@ import React from "react";
 import ResponsiveAppBar from "../components/AccountNavBar";
 import { Link, useNavigate } from "react-router-dom";
 import SideBar from "../components/Sidebar";
-import { Grid , Button } from "@material-ui/core";
-import {EditOutlined} from "@material-ui/icons";
+import { Grid, Button } from "@material-ui/core";
+import { EditOutlined } from "@material-ui/icons";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
-
-
-import {UserAuth} from '../contexts/AuthContext'
-import {db} from '../firebase'
+import { UserAuth } from "../contexts/AuthContext";
+import { db, upload } from "../firebase";
 
 import { doc, getDoc } from "firebase/firestore";
-
-
-
+import { useEffect, useState } from "react";
 
 function UserProfile() {
-    const {user} = UserAuth()
-
-    async function getRecord(){
-        const ref = doc(db,"users", user.uid)
-        const docSnap = await getDoc(ref)
-        if(docSnap.exists()){
-            console.log(docSnap.data())
-            document.getElementById('name').innerText = 'Name: ' + docSnap.data().name
-            document.getElementById('age').innerText = 'Age: ' + docSnap.data().age + " Years Old"
-            document.getElementById('weight').innerText = 'Weight: ' + docSnap.data().weight + " Kg"
-
-        }
-        else{
-            console.log("error")
-        }
+  const { user } = UserAuth();
+  const [photo, setPhoto] = useState(null);
+  const [photoURL, setPhotoURL] = React.useState(
+    "https://www.pngkey.com/png/detail/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png"
+  );
+  const [loading, setLoading] = useState(false);
+  async function getRecord() {
+    const ref = doc(db, "users", user.uid);
+    const docSnap = await getDoc(ref);
+    if (docSnap.exists()) {
+      console.log(docSnap.data());
+      document.getElementById("name").innerText =
+        "Name: " + docSnap.data().name;
+      document.getElementById("age").innerText =
+        "Age: " + docSnap.data().age + " Years Old";
+      document.getElementById("weight").innerText =
+        "Weight: " + docSnap.data().weight + " Kg";
+    } else {
+      console.log("error");
     }
+  }
 
-    getRecord()
+  getRecord();
 
+  function handleChange(e) {
+    if (e.target.files[0]) {
+      setPhoto(e.target.files[0]);
+    }
+  }
+  function handleCLick() {
+    upload(photo, user, setLoading);
+  }
 
+  useEffect(() => {
+    if (user?.photoURL) {
+      setPhotoURL(user.photoURL);
+    }
+  }, [user]);
 
+  return (
+    <div>
+      <div style={{ paddingBottom: 9 }}>
+        <ResponsiveAppBar />
+      </div>
 
+      <Grid
+        container
+        alignItems="flex-start"
+        justify="flex-start"
+        spacing={1}
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={2} sm={6} style={{ padding: 0, maxWidth: 240 }}>
+          <SideBar />
+        </Grid>
 
-    return (
-        <div>
-            <div style={{paddingBottom:9}}>
-            <ResponsiveAppBar />
-            </div>
+        <Grid item xs={1}>
+          <div></div>
+        </Grid>
 
-            <Grid container alignItems="flex-start" justify = 'flex-start' spacing={1} style ={{minHeight:"100vh"}}>
+        <Grid item>
+          <p
+            style={{
+              fontFamily: "inter",
+              fontSize: "32px",
+              fontStyle: "regular",
+              textDecoration: "underline",
+            }}
+          >
+            My Profile
+          </p>
+          <Avatar
+            alt="Avatar"
+            src={photoURL}
+            sx={{ width: 120, height: 120 }}
+          />
+          <div sx={{ display: "inline-flex" }}>
+            <input type="file" onChange={handleChange} />
+            <Button
+              disabled={loading || !photo}
+              variant="contained"
+              style={{ backgroundColor: "Pink" }}
+              onClick={handleCLick}
+            >
+              Update Profile Picture
+            </Button>
+          </div>
+          <p id="name">
+            Name:
+            <Box
+              component="form"
+              sx={{
+                marginLeft: "1rem",
+                display: "inline-flex",
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="outlined-basic"
+                label="Insert Name"
+                variant="standard"
+              />
+            </Box>
+          </p>
+          <p>
+            Email : {user && user.email}
+            <Box
+              component="form"
+              sx={{
+                marginLeft: "2rem",
+                display: "inline-flex",
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="outlined-basic"
+                label="Insert Email"
+                variant="standard"
+              />
+            </Box>
+          </p>
+          <p id="age">
+            Age:
+            <Box
+              component="form"
+              sx={{
+                marginLeft: "1rem",
+                display: "inline-flex",
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="outlined-basic"
+                label="Insert Age"
+                variant="standard"
+              />
+            </Box>
+          </p>
+          <p id="weight">
+            Weight:
+            <Box
+              component="form"
+              sx={{
+                marginLeft: "1rem",
+                display: "inline-flex",
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="outlined-basic"
+                label="Insert Weight"
+                variant="standard"
+              />
+            </Box>
+          </p>
+        </Grid>
 
-                <Grid item xs = {2} sm ={6} style={{ padding: 0 , maxWidth:240 }}>
-                <SideBar />
-                </Grid>
-
-                <Grid item xs = {1}>
-                    <div>
-
-                    </div>
-
-                </Grid>
-
-                <Grid item >
-                    <p style={{fontFamily:"inter", fontSize:'32px' , fontStyle : "regular" , textDecoration: "underline"}}>My Profile</p>
-                    <p>Email : {user && user.email}</p>
-                    <p id = 'name'>name: </p>
-                    <p id = 'age'>Age:</p>
-                    <p id ='weight'>Weight:</p>
-                </Grid>
-
-                <Grid item style={{margin:32}} >
-                    <Button component ={Link} to ='/editprofile' startIcon={<EditOutlined />} >
-                    </Button>
-                </Grid>
-
-
-
-            </Grid>
-
-
-        </div>
-
-
-    )
-
+        <Grid item style={{ margin: 32 }}>
+          <Button
+            style={{ paddingLeft: "20rem" }}
+            component={Link}
+            to="/editprofile"
+            startIcon={<EditOutlined />}
+          ></Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
-export default UserProfile
+export default UserProfile;
