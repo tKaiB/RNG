72fc872138d3215ee5bd57cac2f1b4fb6e38d0bf
Vignesh Ezhil <86967110+viggy2000@users.noreply.github.com>
2022-10-19 18:38:03 +0800

    updated

--- my-app/src/pages/RecipeGenerator.js
+++ my-app/src/pages/RecipeGenerator.js
@@ -1,9 +1,141 @@
 import React, { useRef, useState } from "react";
-import CalorieCard from "../components/CalorieCard";
+import { Grid,Button } from "@material-ui/core";
+import { Card , CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import ResponsiveAppBar from "../components/AccountNavBar";
+import SideBar from "../components/Sidebar";
+
 
 function RecipeGenerator(){
     return(
-        <CalorieCard></CalorieCard>
+        <div>
+            <div style={{ paddingBottom: 10 }}>
+                <ResponsiveAppBar />
+                <div style={{position:'absolute' , right:'40%' , padding:'3rem'}}>
+                        <Button>
+                            Generate Recipe
+                        </Button>
+
+                    </div>
+                
+            </div>
+
+            <div>
+                <Grid container
+                        alignItems="flex-start"
+                        justifyContent="flex-start"
+                        spacing={1}
+                        style={{ minHeight: "100vh" }}
+                >
+                    <Grid item xs={2} sm={6} style={{ padding: 0, maxWidth: 240 }}>
+                        <SideBar />
+                    </Grid>
+
+
+                    <Grid item xs={1} style = {{maxWidth:30}}>
+                        <div>
+
+                        </div>
+                    </Grid>
+
+                    <Grid item xs={3} style={{paddingTop:100}}>
+                         <Card sx = {{maxWidth:360}}>
+                            <CardHeader
+                            title="Maggi Goreng"
+                            subheader = "meal 1"
+                            />
+                {/* 
+                            <CardMedia>
+
+                            </CardMedia> */}
+
+                            <CardContent>
+                                <Grid container spacing = {0}>
+                                    <Grid item xs={6} direction ="column">
+                                        <Typography> Nutrition info</Typography>
+                                    </Grid>
+
+                                    <Grid item xs = {6} direction ="column">
+                                        <Typography>Time to prep meals</Typography>
+                                    </Grid>
+
+                                </Grid>
+                                
+                                
+                            </CardContent>
+                        
+
+                        </Card>
+                    </Grid>
+
+                    <Grid item xs={3} style={{paddingTop:100}}>
+                         <Card sx = {{maxWidth:360}}>
+                            <CardHeader
+                            title="Meal 2"
+                            subheader = "meal 1"
+                            />
+                {/* 
+                            <CardMedia>
+
+                            </CardMedia> */}
+
+                            <CardContent>
+                                <Grid container spacing = {0}>
+                                    <Grid item xs={6} direction ="column">
+                                        <Typography> Nutrition info</Typography>
+                                    </Grid>
+
+                                    <Grid item xs = {6} direction ="column">
+                                        <Typography>Time to prep meals</Typography>
+                                    </Grid>
+
+                                </Grid>
+                                
+                                
+                            </CardContent>
+                        
+
+                        </Card>
+                    </Grid>
+
+                    <Grid item xs={3} style={{paddingTop:100}}>
+                         <Card sx = {{maxWidth:360}}>
+                            <CardHeader
+                            title="Meal 3"
+                            subheader = "meal 1"
+                            />
+                {/* 
+                            <CardMedia>
+
+                            </CardMedia> */}
+
+                            <CardContent>
+                                <Grid container spacing = {0}>
+                                    <Grid item xs={6} direction ="column">
+                                        <Typography> Nutrition info</Typography>
+                                    </Grid>
+
+                                    <Grid item xs = {6} direction ="column">
+                                        <Typography>Time to prep meals</Typography>
+                                    </Grid>
+
+                                </Grid>
+                                
+                                
+                            </CardContent>
+                        
+
+                        </Card>
+                    </Grid>
+
+
+
+
+
+                </Grid>
+            </div>
+        </div>
+
+        
 
     )
 }
