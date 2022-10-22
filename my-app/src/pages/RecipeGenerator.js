36bb221bec28292dd51036c7674410194ee7c0f6
Vignesh Ezhil <86967110+viggy2000@users.noreply.github.com>
2022-10-22 21:45:04 +0800

    expand button completed

--- my-app/src/pages/RecipeGenerator.js
+++ my-app/src/pages/RecipeGenerator.js
@@ -1,13 +1,44 @@
 import React, { useRef, useState } from "react";
-import { Grid, Button } from "@material-ui/core";
+import { Grid, Button, CardActions } from "@material-ui/core";
 import { Card, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import ResponsiveAppBar from "../components/AccountNavBar";
 import SideBar from "../components/Sidebar";
 import CalorieCard from "../components/CalorieCard";
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Collapse from '@mui/material/Collapse';
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
 
 
 function RecipeGenerator() {
     const recipes = { id: 1, name: 'fried rice', calories: 590, description: ['make a choice and proceed with recipe', 'depending on size of squash , cut into half or fourths', 'remove seeds', 'for spicy squash , drizzle olive oil or melted butter over each cut squash piece', 'season with mexican seasoning mix ii', 'for sweet squash , drizzle melted honey , butter , grated piloncillo over each cut squash piece', 'season with sweet mexican spice mix', 'bake at 350 degrees , again depending on size , for 40 minutes up to an hour , until a fork can easily pierce the skin', 'be careful not to burn the squash especially if you opt to use sugar or butter', 'if you feel more comfortable , cover the squash with aluminum foil the first half hour , give or take , of baking', 'if desired , season with salt'] };
+    const [expanded, setExpanded] = useState(false)
+    const [expanded2, setExpanded2] = useState(false)
+    const [expanded3, setExpanded3] = useState(false)
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded)
+      }
+    const handleExpandClick2 = () => {
+        setExpanded2(!expanded2)
+      }
+
+    const handleExpandClick3 = () => {
+        setExpanded3(!expanded3)
+      }
+
+
 
     return (
         <div>
@@ -41,9 +72,9 @@ function RecipeGenerator() {
 
                     <Grid item xs={3} style={{ paddingTop: 100 }}>
                         
-                        <CalorieCard name = {recipes.name} keyId = {recipes.keyId} description = {recipes.description} calories = {recipes.calories}/>
+                        {/* <CalorieCard name = {recipes.name} keyId = {recipes.keyId} description = {recipes.description} calories = {recipes.calories}/> */}
                         {/* How to make it dynamic when changing the recipe */}
-                        {/* <Card sx={{ maxWidth: 360 }}> 
+                        <Card sx={{ maxWidth: 360 }}> 
                             <CardHeader
                                 title="Maggi Goreng"
                                 subheader="meal 1"
@@ -64,11 +95,39 @@ function RecipeGenerator() {
 
                             </CardContent>
 
+                            <CardActions disableSpacing>
+                                <ExpandMore
+                                expand={expanded}
+                                onClick={handleExpandClick}
+                                aria-expanded={expanded}
+                                aria-label="show more"
+                                >
+                                    <ExpandMoreIcon></ExpandMoreIcon>
 
-                        </Card> */}
+                                </ExpandMore>
+
+                            </CardActions>
+
+                            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                                <CardContent>
+                                    <div>
+                                        <Typography>How to make it :</Typography>
+                                    </div>
+
+                                    <div style={{paddingTop:10}}>
+                                        <Typography> Ingredients:</Typography>
+                                    </div>
+
+                                </CardContent>
+                                
+
+                            </Collapse>
+
+
+                        </Card> 
                     </Grid>
 
-                    {/* <Grid item xs={3} style={{ paddingTop: 100 }}>
+                    <Grid item xs={3} style={{ paddingTop: 100 }}>
                         <Card sx={{ maxWidth: 360 }}>
                             <CardHeader
                                 title="Meal 2"
@@ -86,9 +145,37 @@ function RecipeGenerator() {
                                     </Grid>
 
                                 </Grid>
-
-
                             </CardContent>
+
+                            <CardActions disableSpacing>
+                                <ExpandMore
+                                expand={expanded2}
+                                onClick={handleExpandClick2}
+                                aria-expanded={expanded2}
+                                aria-label="show more"
+                                >
+                                    <ExpandMoreIcon></ExpandMoreIcon>
+
+                                </ExpandMore>
+
+                            </CardActions>
+
+                            <Collapse in={expanded2} timeout="auto" unmountOnExit>
+                                <CardContent>
+                                    <div>
+                                        <Typography>How to make it :</Typography>
+                                    </div>
+
+                                    <div style={{paddingTop:10}}>
+                                        <Typography> Ingredients:</Typography>
+                                    </div>
+
+                                </CardContent>
+                                
+
+                            </Collapse>
+
+                            
 
 
                         </Card>
@@ -115,9 +202,37 @@ function RecipeGenerator() {
 
                             </CardContent>
 
+                            <CardActions disableSpacing>
+                                <ExpandMore
+                                expand={expanded3}
+                                onClick={handleExpandClick3}
+                                aria-expanded={expanded3}
+                                aria-label="show more"
+                                >
+                                    <ExpandMoreIcon></ExpandMoreIcon>
+
+                                </ExpandMore>
+
+                            </CardActions>
+
+                            <Collapse in={expanded3} timeout="auto" unmountOnExit>
+                                <CardContent>
+                                    <div>
+                                        <Typography>How to make it :</Typography>
+                                    </div>
+
+                                    <div style={{paddingTop:10}}>
+                                        <Typography> Ingredients:</Typography>
+                                    </div>
+
+                                </CardContent>
+                                
+
+                            </Collapse>
+
 
                         </Card>
-                    </Grid> */}
+                    </Grid> 
 
 
 
